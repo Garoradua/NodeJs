55596f627c883146dc8f8a852f0ebef0b60baf30
Gourav Arora <gouravarora035@gmail.com>
2021-09-30 14:47:30 +0530

    second

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-var fs = require('fs');
-hi
-hello
+var http = require('http');
+// const { type } = require('os');
+
+var server = http.createServer(function(req, res){
+    console.log(req.url);
+    res.writeHead(200, {'Content-Type' : 'text/plain' });
+    res.end("Hello Gourav");
+});
+
+server.listen(2000, '127.0.0.1');
