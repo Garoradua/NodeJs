ca3e28bdb27feb3a62a7292fdb1c0f204fdf9ade
Gourav Arora <gouravarora035@gmail.com>
2021-09-30 15:23:50 +0530

    stream

--- stream.js
+++ stream.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var stream = fs.createReadStream(__dirname + '/file1.txt');
+var stream = fs.createReadStream(__dirname + '/file1.txt', 'utf-8');
 
 stream.on('data', function(chunk123){
     console.log("New chunk found");
